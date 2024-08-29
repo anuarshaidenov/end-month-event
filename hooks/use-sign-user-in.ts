74d64bcd61a40941577793aecaf67bfc042f7206
anuar <a.shaidenov@aix.kz>
2024-08-29 23:29:32 +0500

    Skip redirect

--- hooks/use-sign-user-in.ts
+++ hooks/use-sign-user-in.ts
@@ -16,6 +16,7 @@ export const useSignUserInWithGoogle = () => {
             access_type: "offline",
             prompt: "consent",
           },
+          skipBrowserRedirect: true,
         },
       });
     },
